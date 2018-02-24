f7e857680f3788cde2e8e8f6bf3837256c7d1e9a
Pratyush Poddar <pratyush.poddar@cleartax.in>
2018-02-24 18:13:00 +0530

    made some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/src/pages/blog`,
+        name: 'blogs',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
